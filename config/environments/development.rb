03ed416ccfaa26feee8273ba150c2e1d72fa399f
natron19 <nathan19@hotmail.com>
2015-12-25 17:12:57 -0800

    Adds sendgrid email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,13 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
+
   config.action_controller.perform_caching = false
+
+
+  config.action_mailer.delivery_method = :test
+
+  config.action_mailer.default_url_options = { :host => 'http://yournitrouspreviewurl'}
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
